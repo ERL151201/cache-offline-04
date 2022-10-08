80ff36ad455f1d703c0b0f30317265b78d10f054
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-10-07 19:35:43 -0500

    cache y network race

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ self.addEventListener('install', e => {
             '/index.html',
             '/css/style.css',
             '/img/main.jpg',
-
+            '/img/no-img.jpg',
             '/js/app.js'
         ]);
     });
@@ -48,22 +48,54 @@ self.addEventListener('fetch', e => {
 
 
 
+    //5 - Cache & Network Race
+
+    const respuesta = new Promise((resolve, reject) => {
+
+        let rechazada = false;
+        const falloUnaVez = () => {
+            if (rechazada) {
+                if (/\.(png|jpg)$/i.test(e.request.url)) {
+                    resolve(caches.match('/img/no-image.jpg'));
+                } else {
+                    reject('No se encontro respuesta');
+                }
+            } else {
+                rechazada = true;
+            }
+        };
+
+        fetch(e.request).then(res => {
+            res.ok ? resolve(res) : falloUnaVez();
+        }).catch(falloUnaVez);
+
+        caches.match(e.request).then(res => {
+            res ? resolve(res) : falloUnaVez();
+        }).catch(falloUnaVez);
+
+    });
+
+
+    e.respondWith(respuesta);
+
+
+
     //4 - Cache with network update
     // Rendimiento es critico
     // Siempre estarn a un paso atras
 
-    if (e.request.url.includes('bootstrap')) {
-        return e.respondWith(caches.match(e.request));
-    }
+    // if (e.request.url.includes('bootstrap')) {
+    //     return e.respondWith(caches.match(e.request));
+    // }
 
-    const respuesta = caches.open(CACHE_STATIC_NAME).then(cache => {
-        fetch(e.request).then(newRes =>
-            cache.put(e.request, newRes));
+    // const respuesta = caches.open(CACHE_STATIC_NAME).then(cache => {
+    //     fetch(e.request).then(newRes =>
+    //         cache.put(e.request, newRes));
 
-        return cache.match(e.request);
-    });
+    //     return cache.match(e.request);
+    // });
 
-    e.respondWith(respuesta);
+    // e.respondWith(respuesta);
 
 
 
